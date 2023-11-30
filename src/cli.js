e95b34232a44ca4f5088d284d5b4acb357888894
Astreytal <sekret.vad@gmail.com>
2023-11-30 11:03:58 +0300

    1

--- src/cli.js
+++ src/cli.js
@@ -1,12 +1,7 @@
 import readlineSync from 'readline-sync';
 
-const getName = (main) => {
-    let name
-  if (!main) {
-    name = readlineSync.question('Мay I have your name? ');
-  } else {
-    name = readlineSync.question('/Мay I have your name?/ '); 
-  }
-  return name;
-};
-export default getName;
+const getName = () => {
+    const name = readlineSync.question('Мay I have your name? ')
+    return name
+}
+export default getName

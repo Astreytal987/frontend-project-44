4c39ee9693f4e5da3344c05de0bff55cb908cd81
Astreytal <sekret.vad@gmail.com>
2023-12-02 22:28:36 +0300

    fixPtimeFormat

--- bin/games/brain-prime.js
+++ bin/games/brain-prime.js
@@ -2,7 +2,7 @@
 // тут просто исполняймый файл с правилами вопросами и ответами а также вычисление случайного вопроса и его ответ
 import logicGame from '../../src/index.js';
 
-let rules = "Answer 'yes' if given number is prime. Otherwise answer 'no".        ;
+let rules = "Answer 'yes' if given number is prime. Otherwise answer 'no.";
 
 const expressionFunction = () => {
   let randomNum = Math.floor(Math.random() * 8) + 2; //рандомное число от 1 до 50
